9b37051529ccb632d9795742c6559460ae71ae99
Mohamed Mahmoud Hamed <114697974+mohamed007m@users.noreply.github.com>
2026-01-22 15:43:40 +0200

    basic structure of the program
    The basic structure of the calculator.

--- calculator.py
+++ calculator.py
@@ -1,1 +1,33 @@
 #simple calculator 
+
+#Addition function
+def add(x, y):
+    return 
+
+#Subtraction function
+def sub(x, y):
+    return
+
+
+#Multiplication function
+def mul(x, y):
+    return
+
+#divide function
+def divide(x, y):
+    return
+
+#Power function
+def power(x, y):
+    return
+
+#SquareRoot function
+def sqr(x):
+    return
+
+
+# interface and methodology
+
+def main():
+
+main()
